dfd750560ef671c57d228ada410d107a4cd8d917
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-06-19 02:23:32 +0600

    server version 1

---
+++ server/inngest/index.js
@@ -0,0 +1,50 @@
+import { Inngest } from "inngest";
+import User from "../models/userModel.js"; // Adjust path as needed
+
+// Create Inngest client
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+// 1. User Created
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const userData = {
+      id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      image: image_url,
+    };
+    await User.create(userData);
+  }
+);
+
+// 2. User Deleted
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// 3. User Updated
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+    const userData = {
+      id,
+      email: email_addresses[0].email_address,
+      name: `${first_name} ${last_name}`,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Export all Inngest functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
